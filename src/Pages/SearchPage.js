cd1ef151ef56b98ae058aca1d437cd9ca5622496
Anas Alpure <infomix82@gmail.com>
2018-08-26 13:44:19 +0300

    feat : Add search feature using Keywords to help users find what they search

--- src/Pages/SearchPage.js
+++ src/Pages/SearchPage.js
@@ -3,26 +3,35 @@ import React ,{Component}  from 'react' ;
 import * as BooksAPI from '../BooksAPI'
 import BookItem from '../Components/BookItem'
 
-
+const booksKeywords = [
+    'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen',
+    'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business',
+    'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket',
+    'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama',
+    'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First',
+    'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen',
+    'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage',
+    'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry','Production',
+    'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction','Shakespeare', 'Singh',
+    'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality','Web Development', 'iOS'
+]
 
 class SearchPage extends Component
 {
     state ={
-        query : 'd' ,
         books : [] ,
     }
 
-    componentDidMount(){
-        BooksAPI.getAll()
-                .then( books=>this.setState({
-                    books
-                })
-        )
-       
-    }
 
-    search (event){
-        event.target.value
+    search = ({target})=>{
+        let query = target.value 
+        if(query.length>0)
+            BooksAPI.search(query)
+                .then( books=>{ if(!books.error)this.setState({ books })
+                                else this.setState({ books : [] })
+                    })
+                .catch( err =>this.setState({ books :[] })  )
+        else this.setState({ books :[] })
     }
 
     render (){
@@ -33,10 +42,15 @@ class SearchPage extends Component
                 <div className="search-books-bar">
                     <a className="close-search" onClick= {()=>onAddNewBook('hi') }>Close</a>
                     <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author" onChange={ this.search }/>
+                        <input type="text" placeholder="Search by title or author" onChange={ this.search } list="BooksCats"/>
+                        <datalist id="BooksCats"> 
+                           {booksKeywords.map( cat => <option value={cat} /> ) }
+                        </datalist>
+
                     </div>
                 </div>
                 <div className="search-books-results">
+                   <div> There are {books.length} books available </div>
                     <ol className="books-grid" >  
                         {books && books.map( (book , index )=><li key={index}> <BookItem book={book} /> </li>  )}     
                     </ol>
